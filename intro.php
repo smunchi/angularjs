43ba3c639166a5ee26da4bf843ac07b7fc47aa97
Salahuddin Munchi <salahuddin@cefalo.com>
2015-09-30 12:43:37 +0600

    added ng-click and ng-hide directive to show and hide of listing data

--- intro.php
+++ intro.php
@@ -1,10 +1,10 @@
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 <div ng-app="myApp" ng-controller="myController" style="margin: 0 auto; width: 500px; border: 1px solid activeborder; padding: 10px; margin-top: 50px">
     <div style="color: blue">{{message}}</div>    
-    <br/><span style="color: #003956">Search here  </span><input type="text" ng-model="test"/>
-    <ul>
+    <br/><span style="color: #003956">Search here </span><input type="text" ng-model="test"/> <a id="search" href="javascript:void(0);" ng-click="toggle();">Show all</a>
+    <ul ng-hide="listingData">
         <li style="color: green" ng-repeat="x in names |filter:test| orderBy:'roll'">
-            {{x.name}}
+            {{x.name | uppercase}}
         </li>
     </ul>
 </div>
@@ -12,9 +12,13 @@
 <script>
  var app = angular.module("myApp", []);
  app.controller('myController', function($scope, $http) {
-    $scope.message = 'Welcome to angular js';
+    $scope.message = 'Welcome to angular js. I am gonna make it or break it based on skill that no one ever provide';
+    $scope.listingData = true;
     $http.get("serverScript.php").success(function(response) {
     $scope.names = response;
+    },
+    $scope.toggle = function(){
+       $scope.listingData = !$scope.listingData;
     });
  });
 </script>
